c34dca22424dd4cbcd9a31232e8427546d404f95
DJ_Kovrik <djkovrik@gmail.com>
2017-07-25 18:34:41 +0300

    Add base data model classes

---
+++ app/src/main/java/com/sedsoftware/yaptalker/data/Model.kt
@@ -0,0 +1,17 @@
+package com.sedsoftware.yaptalker.data
+
+data class NewsItem(val summary: String, val topic: TopicItem)
+
+data class ForumItem(val id: Int, val title: String, val image: String, val description: String,
+    val lastTopic: TopicItem)
+
+data class TopicItem(val id: Int, val title: String, val answers: Int, val views: Int, val uq: Int,
+    val author: UserProfileShort, val date: String)
+
+data class PostItem(val id: Int, val author: UserProfileShort, val date: String, val uq: String,
+    val content: String)
+
+data class UserProfileShort(val id: Int, val name: String)
+
+data class UserProfileInfo(val shortInfo: UserProfileShort, val picture: String, val avatar: String,
+    val messagesCount: Int, val uq: Int)
