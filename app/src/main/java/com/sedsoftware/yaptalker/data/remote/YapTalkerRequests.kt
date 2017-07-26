5a7c3bc2d9738622e5e2eded07631aa5a8bec460
DJ_Kovrik <djkovrik@gmail.com>
2017-07-26 19:53:59 +0300

    Tweak request interfaces

--- app/src/main/java/com/sedsoftware/yaptalker/data/remote/YapTalkerRequests.kt
+++ app/src/main/java/com/sedsoftware/yaptalker/data/remote/YapTalkerRequests.kt
@@ -1,31 +1,65 @@
+@file:Suppress("KDocUnresolvedReference")
+
 package com.sedsoftware.yaptalker.data.remote
 
-import com.sedsoftware.yaptalker.data.ForumItem
-import com.sedsoftware.yaptalker.data.NewsItem
-import com.sedsoftware.yaptalker.data.TopicItem
-import com.sedsoftware.yaptalker.data.UserProfileInfo
+import com.sedsoftware.yaptalker.data.*
 import io.reactivex.Observable
 import retrofit2.http.GET
 import retrofit2.http.Path
 
-
+/**
+ * Load news from main page
+ *
+ * @param startPage Starting page number, also defines how much pages will be loaded.
+ */
 interface YapNewsLoader {
   @GET("/st/{startPage}/")
   fun loadNews(@Path("startPage") startPage: Int): Observable<List<NewsItem>>
 }
 
-interface YapForumsLoader {
-  @GET("/forum{forumId}/")
-  fun loadForum(@Path("forumId") forumId: Int): Observable<List<ForumItem>>
+/**
+ * Load main forums list
+ */
+interface YapForumsListLoader {
+  @GET()
+  fun loadForumsList(): Observable<List<ForumItem>>
 }
 
+/**
+ * Load chosen forum topics list
+ *
+ * @param forumId Forum id
+ * @param startFrom Starting topic number, should be multiple of 30
+ * @param sortingMode Topic sorting mode, possible values: last_post and rank
+ */
+interface YapChosenForumLoader {
+  @GET("/forum{forumId}/st/{startFrom}/100/Z-A/{sortingMode}")
+  fun loadChosenForum(
+      @Path("forumId") forumId: Int,
+      @Path("startFrom") startTopicNumber: Int,
+      @Path("sortingMode") sortingMode: String): Observable<List<TopicItem>>
+}
+
+/**
+ * Load chosen topic
+ *
+ * @param forumId Forum id
+ * @param startPage Starting topic page, should be multiple of 25
+ * @param topicId Topic id
+ */
 interface YapTopicLoader {
   @GET("/forum{forumId}/st/{startPage}/topic{topicId}.html")
-  fun loadTopic(@Path("forumId") forumId: Int,
-      @Path("topicId") topicId: Int): Observable<List<TopicItem>>
+  fun loadTopic(
+      @Path("forumId") forumId: Int,
+      @Path("topicId") topicId: Int): Observable<List<PostItem>>
 }
 
+/**
+ * Load user profile info
+ *
+ * @param memberId User profile id
+ */
 interface YapProfileLoader {
   @GET("/members/member{memberId}.html")
-  fun loadUserInfo(@Path("memberId") memberId: Int): Observable<UserProfileInfo>
+  fun loadUserProfile(@Path("memberId") memberId: Int): Observable<UserProfileInfo>
 }
