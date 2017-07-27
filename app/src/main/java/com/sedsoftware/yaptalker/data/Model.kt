0ef4783af95be3ed927804bb2a57cc8ea55873db
DJ_Kovrik <djkovrik@gmail.com>
2017-07-27 19:34:53 +0300

    Remove views field

--- app/src/main/java/com/sedsoftware/yaptalker/data/Model.kt
+++ app/src/main/java/com/sedsoftware/yaptalker/data/Model.kt
@@ -7,7 +7,7 @@ data class ForumItem(val id: Int, val title: String, val lastTopic: TopicItemSho
 data class TopicItemShort(val id: Int, val title: String, val author: UserProfileShort,
     val date: String)
 
-data class TopicItem(val id: Int, val title: String, val answers: Int, val views: Int, val uq: Int,
+data class TopicItem(val id: Int, val title: String, val answers: Int, val uq: Int,
     val author: UserProfileShort, val date: String)
 
 data class PostItem(val id: Int, val author: UserProfileShort, val date: String, val uq: String,
