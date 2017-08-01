2894f92d0c222d1dfa22e0f2396936be29d53cd0
DJ_Kovrik <djkovrik@gmail.com>
2017-08-01 16:33:50 +0300

    Remove debug app class because of too much duplicating code

--- app/src/main/java/com/sedsoftware/yaptalker/YapTalkerApp.kt
+++ app/src/main/java/com/sedsoftware/yaptalker/YapTalkerApp.kt
@@ -1,8 +1,11 @@
 package com.sedsoftware.yaptalker
 
 import android.app.Application
+import com.facebook.stetho.Stetho
 import com.sedsoftware.yaptalker.di.ApplicationComponent
 import com.sedsoftware.yaptalker.di.DaggerApplicationComponent
+import com.squareup.leakcanary.LeakCanary
+import timber.log.Timber
 
 class YapTalkerApp : Application() {
 
@@ -13,7 +16,23 @@ class YapTalkerApp : Application() {
   override fun onCreate() {
     super.onCreate()
 
-    // TODO() Init Timber with Firebase Crash Reporting here
+    if (LeakCanary.isInAnalyzerProcess(this)) {
+      // This process is dedicated to LeakCanary for heap analysis.
+      // You should not init your app in this process.
+      return
+    }
+
+    LeakCanary.install(this)
+
+    Timber.uprootAll()
+
+    if (BuildConfig.DEBUG) {
+      LeakCanary.install(this)
+      Stetho.initializeWithDefaults(this)
+      Timber.plant(Timber.DebugTree())
+    } else {
+      // TODO() Init Timber with Firebase Crash Reporting tree here
+    }
 
     appComponent = DaggerApplicationComponent.builder().build()
   }
