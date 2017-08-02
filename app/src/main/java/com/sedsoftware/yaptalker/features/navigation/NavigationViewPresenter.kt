65d39c83af2e181b581631d555c83fd10ec70e10
DJ_Kovrik <djkovrik@gmail.com>
2017-08-02 14:43:33 +0300

    Add missed presenter annotation

--- app/src/main/java/com/sedsoftware/yaptalker/features/navigation/NavigationViewPresenter.kt
+++ app/src/main/java/com/sedsoftware/yaptalker/features/navigation/NavigationViewPresenter.kt
@@ -1,7 +1,9 @@
 package com.sedsoftware.yaptalker.features.navigation
 
+import com.arellomobile.mvp.InjectViewState
 import com.sedsoftware.yaptalker.features.base.BasePresenter
 
+@InjectViewState
 class NavigationViewPresenter : BasePresenter<NavigationView>() {
 
 }
