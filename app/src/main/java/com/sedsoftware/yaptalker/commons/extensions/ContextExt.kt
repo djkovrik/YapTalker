45b3a6a794d643bc92cc5ab6df356d2b7f002b17
DJ_Kovrik <djkovrik@gmail.com>
2017-08-02 18:35:05 +0300

    Add colors for navigation drawer items

--- app/src/main/java/com/sedsoftware/yaptalker/commons/extensions/ContextExt.kt
+++ app/src/main/java/com/sedsoftware/yaptalker/commons/extensions/ContextExt.kt
@@ -2,7 +2,10 @@ package com.sedsoftware.yaptalker.commons.extensions
 
 import android.content.Context
 import android.support.annotation.BoolRes
+import android.support.annotation.ColorRes
 import android.support.annotation.StringRes
+import android.support.v4.content.ContextCompat
 
 fun Context.booleanRes(@BoolRes resId: Int) = resources.getBoolean(resId)
 fun Context.stringRes(@StringRes resId: Int) = resources.getString(resId)
+fun Context.color(@ColorRes colorId: Int) = ContextCompat.getColor(this, colorId)
