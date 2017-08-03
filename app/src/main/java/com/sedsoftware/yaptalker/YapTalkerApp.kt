2e0ec62f67ca69f10d323dbb63e58e640dab45c3
DJ_Kovrik <djkovrik@gmail.com>
2017-08-03 12:55:51 +0300

    Initialize DrawerImageLoader

--- app/src/main/java/com/sedsoftware/yaptalker/YapTalkerApp.kt
+++ app/src/main/java/com/sedsoftware/yaptalker/YapTalkerApp.kt
@@ -1,10 +1,18 @@
 package com.sedsoftware.yaptalker
 
 import android.app.Application
+import android.content.Context
+import android.graphics.drawable.Drawable
+import android.net.Uri
+import android.widget.ImageView
 import com.facebook.stetho.Stetho
+import com.mikepenz.materialdrawer.util.AbstractDrawerImageLoader
+import com.mikepenz.materialdrawer.util.DrawerImageLoader
+import com.mikepenz.materialdrawer.util.DrawerUIUtils
 import com.sedsoftware.yaptalker.di.ApplicationComponent
 import com.sedsoftware.yaptalker.di.DaggerApplicationComponent
 import com.squareup.leakcanary.LeakCanary
+import com.squareup.picasso.Picasso
 import timber.log.Timber
 
 class YapTalkerApp : Application() {
@@ -35,5 +43,19 @@ class YapTalkerApp : Application() {
     }
 
     appComponent = DaggerApplicationComponent.builder().build()
+
+    // Init MaterialDrawer image loader
+
+    DrawerImageLoader.init(object: AbstractDrawerImageLoader() {
+      override fun set(imageView: ImageView?, uri: Uri?, placeholder: Drawable?, tag: String?) {
+        Picasso.with(imageView?.context).load(uri).placeholder(placeholder).into(imageView)
+      }
+
+      override fun cancel(imageView: ImageView?) {
+        Picasso.with(imageView?.context).cancelRequest(imageView)
+      }
+
+      override fun placeholder(ctx: Context?, tag: String?) = DrawerUIUtils.getPlaceHolder(ctx)
+    })
   }
 }
