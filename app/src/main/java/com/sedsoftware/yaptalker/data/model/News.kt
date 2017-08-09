2de190594fa21eeb0360dbf2aae28dc307fe7f6e
DJ_Kovrik <djkovrik@gmail.com>
2017-08-09 13:13:25 +0300

    Add content class for NewsItem

--- app/src/main/java/com/sedsoftware/yaptalker/data/model/News.kt
+++ app/src/main/java/com/sedsoftware/yaptalker/data/model/News.kt
@@ -1,3 +1,39 @@
 package com.sedsoftware.yaptalker.data.model
 
-data class NewsItem(val summary: String, val forum: String, val topic: TopicItemList)
+import org.jsoup.Jsoup
+import org.jsoup.safety.Whitelist
+
+data class NewsItem(
+    val summary: String,
+    val forum: String,
+    val topic: TopicItemList)
+
+data class NewsItemContent(val sourceHtml: String) {
+
+  companion object {
+    private val IMAGE_SELECTOR = "img[src]"
+    private val VIDEO_SELECTOR = "iframe[src]"
+    private val SRC_SELECTOR = "src"
+  }
+
+  var images: MutableList<String> = ArrayList()
+  var videos: MutableList<String> = ArrayList()
+
+  var text: String = Jsoup
+      .clean(sourceHtml, Whitelist().addTags("i", "u", "br"))
+      .replace("<br>(\\s+)?\\R<br>", "<br>")
+
+  init {
+    val content = Jsoup.parse(sourceHtml)
+    val imageLinks = content.select(IMAGE_SELECTOR)
+    val videoLinks = content.select(VIDEO_SELECTOR)
+
+    imageLinks.forEach {
+      images.add(it.attr(SRC_SELECTOR))
+    }
+
+    videoLinks.forEach {
+      videos.add(it.html())
+    }
+  }
+}
