1c9c4e8a089d5065987639b48c6541c496d88e00
DJ_Kovrik <djkovrik@gmail.com>
2017-08-12 14:41:51 +0300

    Implement forums presenter

--- app/src/main/java/com/sedsoftware/yaptalker/di/ApplicationComponent.kt
+++ app/src/main/java/com/sedsoftware/yaptalker/di/ApplicationComponent.kt
@@ -3,6 +3,7 @@ package com.sedsoftware.yaptalker.di
 import com.sedsoftware.yaptalker.di.modules.ApplicationModule
 import com.sedsoftware.yaptalker.di.modules.DataManagerModule
 import com.sedsoftware.yaptalker.di.modules.ThumbnailsLoaderModule
+import com.sedsoftware.yaptalker.features.forumslist.ForumsPresenter
 import com.sedsoftware.yaptalker.features.news.NewsAdapter
 import com.sedsoftware.yaptalker.features.news.NewsPresenter
 import dagger.Component
@@ -20,4 +21,5 @@ interface ApplicationComponent {
   // Injections here
   fun inject(presenter: NewsPresenter)
   fun inject(adapter: NewsAdapter)
+  fun inject(presenter: ForumsPresenter)
 }
--- app/src/main/java/com/sedsoftware/yaptalker/features/forumslist/ForumsPresenter.kt
+++ app/src/main/java/com/sedsoftware/yaptalker/features/forumslist/ForumsPresenter.kt
@@ -1,3 +1,49 @@
 package com.sedsoftware.yaptalker.features.forumslist
 
-class ForumsPresenter
+import com.arellomobile.mvp.InjectViewState
+import com.sedsoftware.yaptalker.YapTalkerApp
+import com.sedsoftware.yaptalker.data.model.ForumItem
+import com.sedsoftware.yaptalker.data.remote.yap.YapDataManager
+import com.sedsoftware.yaptalker.features.base.BasePresenter
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.schedulers.Schedulers
+import javax.inject.Inject
+
+@InjectViewState
+class ForumsPresenter : BasePresenter<ForumsView>() {
+
+  @Inject
+  lateinit var yapDataManager: YapDataManager
+
+  init {
+    YapTalkerApp.appComponent.inject(this)
+  }
+
+  fun loadForumsList() {
+
+    val subscription =
+        yapDataManager
+            .getForumsList()
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe({
+              // onSuccess
+              forumsList: List<ForumItem> ->
+              onLoadingSuccess(forumsList)
+            }, {
+              // onError
+              throwable ->
+              onLoadingError(throwable)
+            })
+
+    unsubscribeOnDestroy(subscription)
+  }
+
+  fun onLoadingSuccess(forums: List<ForumItem>) {
+    viewState.showForums(forums)
+  }
+
+  fun onLoadingError(error: Throwable) {
+    error.message?.let { viewState.showErrorMessage(it) }
+  }
+}
