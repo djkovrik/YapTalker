96aebfe78cfe00c3098505ff7d3d6da5c73a3f05
DJ_Kovrik <djkovrik@gmail.com>
2017-08-12 14:41:39 +0300

    Define ForumsView interface

--- app/src/main/java/com/sedsoftware/yaptalker/features/forumslist/ForumsView.kt
+++ app/src/main/java/com/sedsoftware/yaptalker/features/forumslist/ForumsView.kt
@@ -1,3 +1,14 @@
 package com.sedsoftware.yaptalker.features.forumslist
 
-interface ForumsView
+import com.arellomobile.mvp.MvpView
+import com.arellomobile.mvp.viewstate.strategy.AddToEndSingleStrategy
+import com.arellomobile.mvp.viewstate.strategy.StateStrategyType
+import com.sedsoftware.yaptalker.data.model.ForumItem
+
+@StateStrategyType(AddToEndSingleStrategy::class)
+interface ForumsView : MvpView {
+
+  fun showForums(forums: List<ForumItem>)
+
+  fun showErrorMessage(message: String)
+}
