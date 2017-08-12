930dafbee1cf049aa649d3f533556ccf4d62cb2e
DJ_Kovrik <djkovrik@gmail.com>
2017-08-12 12:10:28 +0300

    Add scope provider ext property for Controller

--- app/src/main/java/com/sedsoftware/yaptalker/commons/extensions/ControllerExt.kt
+++ app/src/main/java/com/sedsoftware/yaptalker/commons/extensions/ControllerExt.kt
@@ -2,6 +2,9 @@ package com.sedsoftware.yaptalker.commons.extensions
 
 import android.widget.Toast
 import com.bluelinelabs.conductor.Controller
+import com.bluelinelabs.conductor.autodispose.ControllerEvent
+import com.bluelinelabs.conductor.autodispose.ControllerScopeProvider
+import com.uber.autodispose.LifecycleScopeProvider
 import es.dmoral.toasty.Toasty
 
 fun Controller.toastError(message: String) {
@@ -27,3 +30,6 @@ fun Controller.toastWarning(message: String) {
     Toasty.warning(it.context, message, Toast.LENGTH_SHORT, true).show()
   }
 }
+
+val Controller.scopeProvider: LifecycleScopeProvider<ControllerEvent>
+  get() = ControllerScopeProvider.from(this)
