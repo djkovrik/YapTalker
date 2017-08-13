01ad1c34f66b6fa7473ba0d942a0bf9a06dbaa51
DJ_Kovrik <djkovrik@gmail.com>
2017-08-13 16:25:19 +0300

    Add String ext to shorten the date

--- app/src/main/java/com/sedsoftware/yaptalker/commons/extensions/StringExt.kt
+++ app/src/main/java/com/sedsoftware/yaptalker/commons/extensions/StringExt.kt
@@ -1,5 +1,7 @@
 package com.sedsoftware.yaptalker.commons.extensions
 
+import java.text.SimpleDateFormat
+import java.util.*
 import java.util.regex.Pattern
 
 /**
@@ -26,3 +28,50 @@ fun String.chopEdges() = this.substring(1, this.length - 1)
  * Extracts and returns date string from forum description html.
  */
 fun String.extractDate() = this.substring(0, this.indexOf("<br"))
+
+/**
+ * Converts date string to shortened variant.
+ */
+fun String.getShortTime(): String {
+
+  val format = SimpleDateFormat("dd.MM.yyyy - HH:mm", Locale.getDefault())
+  val topicDate = format.parse(this)
+  val currentDate = Calendar.getInstance().time
+
+  var diffInSeconds = ((currentDate.time - topicDate.time) / 1000).toInt()
+
+  val sec = if (diffInSeconds >= 60) (diffInSeconds % 60) else diffInSeconds
+  diffInSeconds /= 60
+  val min = if (diffInSeconds >= 60) (diffInSeconds % 60) else diffInSeconds
+  diffInSeconds /= 60
+  val hrs = if (diffInSeconds >= 24) (diffInSeconds % 24) else diffInSeconds
+  diffInSeconds /= 24
+  val days = if (diffInSeconds >= 30) (diffInSeconds % 30) else diffInSeconds
+  diffInSeconds /= 30
+  val months = if (diffInSeconds >= 12) (diffInSeconds % 12) else diffInSeconds
+  diffInSeconds /= 12
+  val years = diffInSeconds
+
+  // TODO() Adapt to Russian
+  if (years > 0)
+    return "$years year(s)"
+  else if (months > 0)
+    return "$months month(s)"
+  else if (days > 0)
+    return "$days day(s)"
+  else if (hrs > 0)
+    return "$hrs hour(s)"
+  else if (min > 0) {
+    if (min == 1) {
+      return "minute ago"
+    } else {
+      return "$min min(s)"
+    }
+  } else {
+    if (sec <= 1) {
+      return "second ago"
+    } else {
+      return "$sec seconds"
+    }
+  }
+}
