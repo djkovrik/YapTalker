6e73f3186788317e76e3276e20ce8e88db0855e9
DJ_Kovrik <djkovrik@gmail.com>
2017-08-13 12:39:10 +0300

    Implement forums list loading

--- app/src/main/java/com/sedsoftware/yaptalker/features/forumslist/ForumsAdapter.kt
+++ app/src/main/java/com/sedsoftware/yaptalker/features/forumslist/ForumsAdapter.kt
@@ -1,3 +1,45 @@
 package com.sedsoftware.yaptalker.features.forumslist
 
-class ForumsAdapter
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.sedsoftware.yaptalker.R
+import com.sedsoftware.yaptalker.data.model.ForumItem
+import kotlinx.android.synthetic.main.controller_forums_list_item.view.*
+
+class ForumsAdapter : RecyclerView.Adapter<ForumsAdapter.ForumsViewHolder>() {
+
+  private var forumsList: ArrayList<ForumItem> = ArrayList()
+
+  override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ForumsViewHolder {
+    val view = LayoutInflater.from(parent.context).inflate(R.layout.controller_forums_list_item,
+        parent, false)
+    return ForumsViewHolder(view)
+  }
+
+  override fun onBindViewHolder(holder: ForumsViewHolder, position: Int) {
+    holder.bindTo(forumsList[position])
+  }
+
+  override fun getItemCount() = forumsList.size
+
+  override fun getItemId(position: Int) = forumsList[position].id.toLong()
+
+  fun addForumsList(list: List<ForumItem>) {
+    forumsList.clear()
+    forumsList.addAll(list)
+    notifyDataSetChanged()
+  }
+
+  fun getForumsList() = forumsList
+
+  class ForumsViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+    fun bindTo(forum: ForumItem) {
+      with(itemView) {
+        forum_name.text = forum.title
+      }
+    }
+  }
+}
--- app/src/main/java/com/sedsoftware/yaptalker/features/forumslist/ForumsController.kt
+++ app/src/main/java/com/sedsoftware/yaptalker/features/forumslist/ForumsController.kt
@@ -1,22 +1,51 @@
 package com.sedsoftware.yaptalker.features.forumslist
 
 import android.os.Bundle
+import android.support.v7.widget.LinearLayoutManager
 import android.view.View
+import com.arellomobile.mvp.presenter.InjectPresenter
 import com.sedsoftware.yaptalker.R
+import com.sedsoftware.yaptalker.commons.extensions.toastError
 import com.sedsoftware.yaptalker.data.model.ForumItem
 import com.sedsoftware.yaptalker.features.base.BaseController
+import kotlinx.android.synthetic.main.controller_forums_list.view.*
 
 class ForumsController : BaseController(), ForumsView {
+
+  @InjectPresenter
+  lateinit var forumsPresenter: ForumsPresenter
+
+  lateinit var forumsAdapter: ForumsAdapter
 
   override val controllerLayoutId: Int
     get() = R.layout.controller_forums_list
 
+  // TODO() Check why onViewBound called twice
   override fun onViewBound(view: View, savedViewState: Bundle?) {
+
+    forumsAdapter = ForumsAdapter()
+    forumsAdapter.setHasStableIds(true)
+
+    with(view.forums_list) {
+      val linearLayout = LinearLayoutManager(context)
+      layoutManager = linearLayout
+      adapter = forumsAdapter
+      setHasFixedSize(true)
+    }
+
+    forumsPresenter.loadForumsList()
+  }
+
+  override fun onDetach(view: View) {
+    super.onDetach(view)
+    view.forums_list.adapter = null
   }
 
   override fun showForums(forums: List<ForumItem>) {
+    forumsAdapter.addForumsList(forums)
   }
 
   override fun showErrorMessage(message: String) {
+    toastError(message)
   }
 }
