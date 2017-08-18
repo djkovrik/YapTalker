6570416233d21be3a1eeddec3d25ffc795084a81
DJ_Kovrik <djkovrik@gmail.com>
2017-08-18 20:49:22 +0300

    Define forum view interface

---
+++ app/src/main/java/com/sedsoftware/yaptalker/features/forum/ChosenForumView.kt
@@ -0,0 +1,18 @@
+package com.sedsoftware.yaptalker.features.forum
+
+import com.arellomobile.mvp.MvpView
+import com.arellomobile.mvp.viewstate.strategy.AddToEndSingleStrategy
+import com.arellomobile.mvp.viewstate.strategy.StateStrategyType
+import com.sedsoftware.yaptalker.data.model.Topic
+
+@StateStrategyType(AddToEndSingleStrategy::class)
+interface ChosenForumView : MvpView {
+
+  fun showRefreshing()
+
+  fun hideRefreshing()
+
+  fun showErrorMessage(message: String)
+
+  fun refreshTopics(topics: List<Topic>)
+}
