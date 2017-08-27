2d5668f9fcf11e34dd6b35aa2c09246ae2d1cd57
DJ_Kovrik <djkovrik@gmail.com>
2017-08-27 14:52:08 +0300

    More style tweaks

--- app/src/main/java/com/sedsoftware/yaptalker/features/topic/ChosenTopicAdapter.kt
+++ app/src/main/java/com/sedsoftware/yaptalker/features/topic/ChosenTopicAdapter.kt
@@ -1,6 +1,5 @@
 package com.sedsoftware.yaptalker.features.topic
 
-import android.annotation.SuppressLint
 import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
@@ -8,8 +7,10 @@ import android.view.ViewGroup
 import com.sedsoftware.yaptalker.R
 import com.sedsoftware.yaptalker.commons.extensions.getShortTime
 import com.sedsoftware.yaptalker.commons.extensions.loadAvatarFromUrl
+import com.sedsoftware.yaptalker.commons.extensions.textColor
 import com.sedsoftware.yaptalker.data.model.TopicPost
 import kotlinx.android.synthetic.main.controller_chosen_topic_item.view.*
+import java.util.Locale
 
 class ChosenTopicAdapter : RecyclerView.Adapter<ChosenTopicAdapter.PostViewHolder>() {
 
@@ -46,18 +47,34 @@ class ChosenTopicAdapter : RecyclerView.Adapter<ChosenTopicAdapter.PostViewHolde
 
   inner class PostViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
 
-    @SuppressLint("SetJavaScriptEnabled")
     fun bindTo(postItem: TopicPost) {
       with(postItem) {
         with(itemView) {
           post_author.text = authorNickname
-          post_rating.text = postRank
           post_date.text = context.getShortTime(postDate)
           post_author_avatar.loadAvatarFromUrl("http:$authorAvatar")
+
+          if (postRank.isNotEmpty()) {
+            when {
+              postRank.toInt() > 0 -> {
+                post_rating.text = String.format(Locale.getDefault(), "+%s", postRank)
+                post_rating.textColor = R.color.colorRatingGreen
+              }
+              postRank.toInt() < 0 -> {
+                post_rating.text = String.format(Locale.getDefault(), "-%s", postRank)
+                post_rating.textColor = R.color.colorRatingRed
+              }
+              else -> {
+                post_rating.text = postRank
+                post_rating.textColor = R.color.colorSupportingText
+              }
+            }
+          }
+
           // TODO() Replace webview with parsing
-          post_content.settings.javaScriptEnabled = true
-          post_content.loadDataWithBaseURL("http://www.yaplakal.com/", postContent,
-              "text/html; charset=UTF-8", null, null)
+//          post_content.settings.javaScriptEnabled = true
+//          post_content.loadDataWithBaseURL("http://www.yaplakal.com/", postContent,
+//              "text/html; charset=UTF-8", null, null)
         }
       }
     }
