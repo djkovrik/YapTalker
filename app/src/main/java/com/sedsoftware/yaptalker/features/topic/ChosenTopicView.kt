127c8985369ad6330ef7e85629226d00009fc1fe
DJ_Kovrik <djkovrik@gmail.com>
2017-08-27 14:03:28 +0300

    Define chosen topic view

---
+++ app/src/main/java/com/sedsoftware/yaptalker/features/topic/ChosenTopicView.kt
@@ -0,0 +1,28 @@
+package com.sedsoftware.yaptalker.features.topic
+
+import com.arellomobile.mvp.MvpView
+import com.arellomobile.mvp.viewstate.strategy.AddToEndSingleStrategy
+import com.arellomobile.mvp.viewstate.strategy.StateStrategyType
+import com.sedsoftware.yaptalker.data.model.TopicPost
+
+@StateStrategyType(AddToEndSingleStrategy::class)
+interface ChosenTopicView : MvpView {
+
+  fun showRefreshing()
+
+  fun hideRefreshing()
+
+  fun showErrorMessage(message: String)
+
+  fun refreshPosts(posts: List<TopicPost>)
+
+  fun setNavigationPagesLabel(page: Int, totalPages: Int)
+
+  fun setIfNavigationBackEnabled(isEnabled: Boolean)
+
+  fun setIfNavigationForwardEnabled(isEnabled: Boolean)
+
+  fun showGoToPageDialog(maxPages: Int)
+
+  fun showCantLoadPageMessage(page: Int)
+}
