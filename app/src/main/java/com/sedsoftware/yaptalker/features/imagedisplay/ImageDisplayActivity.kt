a0e7201371ddf030dc5234c926dea330f31cba67
DJ_Kovrik <djkovrik@gmail.com>
2017-09-06 18:30:28 +0300

    Implement images sharing

--- app/src/main/java/com/sedsoftware/yaptalker/features/imagedisplay/ImageDisplayActivity.kt
+++ app/src/main/java/com/sedsoftware/yaptalker/features/imagedisplay/ImageDisplayActivity.kt
@@ -1,7 +1,14 @@
 package com.sedsoftware.yaptalker.features.imagedisplay
 
+import android.content.Intent
+import android.graphics.Bitmap
+import android.graphics.drawable.Drawable
+import android.net.Uri
 import android.os.Bundle
+import android.os.Environment
+import android.support.v4.content.FileProvider
 import android.view.Menu
+import android.view.MenuItem
 import android.view.View
 import android.view.animation.AccelerateInterpolator
 import com.arellomobile.mvp.MvpAppCompatActivity
@@ -10,8 +17,17 @@ import com.sedsoftware.yaptalker.R
 import com.sedsoftware.yaptalker.commons.extensions.hideBeyondScreenEdge
 import com.sedsoftware.yaptalker.commons.extensions.loadFromUrl
 import com.sedsoftware.yaptalker.commons.extensions.showFromScreenEdge
+import com.sedsoftware.yaptalker.commons.extensions.stringRes
+import com.squareup.picasso.Picasso
+import io.reactivex.Single
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.schedulers.Schedulers
 import kotlinx.android.synthetic.main.activity_image_display.*
 import kotlinx.android.synthetic.main.include_main_appbar.*
+import java.io.File
+import java.io.FileOutputStream
+import java.io.IOException
+import com.squareup.picasso.Target as ImageTarget
 
 class ImageDisplayActivity : MvpAppCompatActivity(), ImageDisplayView {
 
@@ -44,6 +60,20 @@ class ImageDisplayActivity : MvpAppCompatActivity(), ImageDisplayView {
     return true
   }
 
+  override fun onOptionsItemSelected(item: MenuItem): Boolean {
+
+    return when (item.itemId) {
+      R.id.action_share -> {
+        shareImage()
+        true
+      }
+      R.id.action_save -> {
+        true
+      }
+      else -> super.onOptionsItemSelected(item)
+    }
+  }
+
   override fun toggleSystemUiVisibility() {
     val uiOptions = window.decorView.systemUiVisibility
     var newUiOptions = uiOptions
@@ -61,5 +91,63 @@ class ImageDisplayActivity : MvpAppCompatActivity(), ImageDisplayView {
 
   override fun showAppbar() {
     appbar?.showFromScreenEdge(AccelerateInterpolator())
+  }
+
+  private fun shareImage() {
+    if (imageUrl.isNotEmpty()) {
+      Picasso
+          .with(this)
+          .load(imageUrl)
+          .into(ShareTarget())
+    }
+  }
+
+  private fun getBitmapUriSingle(bmp: Bitmap): Single<Uri> {
+
+    return Single.create { emitter ->
+      val bmpUri: Uri?
+      try {
+
+        val file = File(getExternalFilesDir(Environment.DIRECTORY_PICTURES),
+            imageUrl.substringAfterLast("/").substringBefore(".") + ".png")
+
+        val out = FileOutputStream(file)
+        bmp.compress(Bitmap.CompressFormat.PNG, 100, out)
+        out.close()
+
+        bmpUri = FileProvider.getUriForFile(this, applicationContext.packageName, file)
+
+        emitter.onSuccess(bmpUri)
+      } catch (e: IOException) {
+        emitter.onError(e)
+      }
+    }
+  }
+
+  private inner class ShareTarget : ImageTarget {
+    override fun onPrepareLoad(placeHolderDrawable: Drawable?) {
+    }
+
+    override fun onBitmapFailed(errorDrawable: Drawable?) {
+    }
+
+    override fun onBitmapLoaded(bitmap: Bitmap, from: Picasso.LoadedFrom?) {
+
+      // TODO() Manage subscription
+      getBitmapUriSingle(bitmap)
+          .observeOn(Schedulers.io())
+          .subscribeOn(AndroidSchedulers.mainThread())
+          .subscribe({ uri ->
+            // onSuccess
+            val intent = Intent(Intent.ACTION_SEND)
+            intent.type = "image/png"
+            intent.putExtra(Intent.EXTRA_STREAM, uri)
+            startActivity(Intent.createChooser(intent, stringRes(R.string.title_share_image)))
+          }, { _ ->
+            // onError
+
+          }
+          )
+    }
   }
 }
