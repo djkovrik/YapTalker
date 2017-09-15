b19fad268e4cadfe8c5966d68d1eb06390f1e292
DJ_Kovrik <djkovrik@gmail.com>
2017-09-15 17:01:44 +0300

    Add dynamic preference summary setup

--- app/src/main/java/com/sedsoftware/yaptalker/features/settings/SettingsActivity.kt
+++ app/src/main/java/com/sedsoftware/yaptalker/features/settings/SettingsActivity.kt
@@ -1,8 +1,12 @@
 package com.sedsoftware.yaptalker.features.settings
 
+import android.content.SharedPreferences
 import android.content.res.Configuration
 import android.os.Bundle
+import android.preference.Preference
 import android.preference.PreferenceActivity
+import android.preference.PreferenceCategory
+import android.preference.PreferenceGroup
 import android.support.annotation.LayoutRes
 import android.support.annotation.Nullable
 import android.support.v7.app.ActionBar
@@ -12,14 +16,23 @@ import android.view.MenuInflater
 import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
+import com.afollestad.materialdialogs.prefs.MaterialListPreference
 import com.sedsoftware.yaptalker.R
 
 // TODO() Replace with controller maybe?
-// TODO() Add dynamic summary setup
-class SettingsActivity : PreferenceActivity() {
+// TODO() Investigate deprecation
+class SettingsActivity : PreferenceActivity(), SharedPreferences.OnSharedPreferenceChangeListener {
 
   private val delegate by lazy {
     AppCompatDelegate.create(this, null)
+  }
+
+  private val sharedPreferences by lazy {
+    preferenceScreen.sharedPreferences
+  }
+
+  private val prefScreen by lazy {
+    preferenceScreen
   }
 
   override fun onCreate(savedInstanceState: Bundle?) {
@@ -28,6 +41,39 @@ class SettingsActivity : PreferenceActivity() {
     super.onCreate(savedInstanceState)
 
     addPreferencesFromResource(R.xml.preferences)
+
+    val count = prefScreen.preferenceCount
+
+    for (i in 0 until count) {
+
+      val category = prefScreen.getPreference(i)
+
+      if (category is PreferenceCategory) {
+        val group = category as PreferenceGroup
+        val groupCount = group.preferenceCount
+
+        (0 until groupCount)
+            .map { group.getPreference(it) }
+            .forEach { setListPreferenceSummary(sharedPreferences, it) }
+      }
+    }
+
+    sharedPreferences.registerOnSharedPreferenceChangeListener(this)
+  }
+
+  override fun onSharedPreferenceChanged(sharedPreferences: SharedPreferences, key: String) {
+    val preference = findPreference(key)
+    setListPreferenceSummary(sharedPreferences, preference)
+  }
+
+  private fun setListPreferenceSummary(sharedPreferences: SharedPreferences, pref: Preference) {
+    if (pref is MaterialListPreference) {
+      val value = sharedPreferences.getString(pref.getKey(), "")
+      val index = pref.findIndexOfValue(value)
+      if (index >= 0) {
+        pref.setSummary(pref.entries[index])
+      }
+    }
   }
 
   override fun onOptionsItemSelected(item: MenuItem): Boolean {
@@ -96,6 +142,7 @@ class SettingsActivity : PreferenceActivity() {
   override fun onDestroy() {
     super.onDestroy()
     delegate.onDestroy()
+    sharedPreferences.unregisterOnSharedPreferenceChangeListener(this)
   }
 
   override fun invalidateOptionsMenu() {
