b5251df9c796f8f7938aef1857483809ea2e4e02
DJ_Kovrik <djkovrik@gmail.com>
2017-09-17 16:20:10 +0300

    Fix preferences default values

--- app/src/main/java/com/sedsoftware/yaptalker/features/settings/SettingsReader.kt
+++ app/src/main/java/com/sedsoftware/yaptalker/features/settings/SettingsReader.kt
@@ -23,32 +23,36 @@ class SettingsReader(val context: Context) {
     context.defaultSharedPreferences
   }
 
+  private val defaultCategories by lazy {
+    context.resources.getStringArray(R.array.pref_categorizer_values).toSet()
+  }
+
   fun getStartingPage(): BaseController {
-    val current = getString(R.string.pref_key_start_page)
-    val main = context.stringRes(R.string.pref_general_start_page_value_main)
+    val current = getString(R.string.pref_key_start_page, "")
+    val forums = context.stringRes(R.string.pref_general_start_page_value_forums)
 
     return when (current) {
-      main -> NewsController()
-      else -> ForumsController()
+      forums -> ForumsController()
+      else -> NewsController()
     }
   }
 
-  fun getNormalFontSize() = getString(R.string.pref_key_font_size).toFloat()
-  fun getBigFontSize() = getString(R.string.pref_key_font_size).toFloat() + TEXT_SIZE_OFFSET
-  fun getSmallFontSize() = getString(R.string.pref_key_font_size).toFloat() - TEXT_SIZE_OFFSET
-  fun getNewsCategories() = getStringSet(R.string.pref_key_categorizer)
-  fun isNsfwEnabled() = getBoolean(R.string.pref_key_nswf)
+  fun getNormalFontSize() = getString(R.string.pref_key_font_size, "14").toFloat()
+  fun getBigFontSize() = getString(R.string.pref_key_font_size, "16").toFloat() + TEXT_SIZE_OFFSET
+  fun getSmallFontSize() = getString(R.string.pref_key_font_size, "12").toFloat() - TEXT_SIZE_OFFSET
+  fun getNewsCategories() = getStringSet(R.string.pref_key_categorizer, defaultCategories)
+  fun isNsfwEnabled() = getBoolean(R.string.pref_key_nswf, false)
 
-  private fun getString(@StringRes key: Int): String {
-    return preferences.getString(context.stringRes(key), "")
+  private fun getString(@StringRes key: Int, default: String): String {
+    return preferences.getString(context.stringRes(key), default)
   }
 
-  private fun getStringSet(@StringRes key: Int): Set<String> {
-    return preferences.getStringSet(context.stringRes(key), HashSet<String>())
+  private fun getStringSet(@StringRes key: Int, default: Set<String>): Set<String> {
+    return preferences.getStringSet(context.stringRes(key), default)
   }
 
-  private fun getBoolean(@StringRes key: Int): Boolean {
-    return preferences.getBoolean(context.stringRes(key), false)
+  private fun getBoolean(@StringRes key: Int, default: Boolean): Boolean {
+    return preferences.getBoolean(context.stringRes(key), default)
   }
 }
 
