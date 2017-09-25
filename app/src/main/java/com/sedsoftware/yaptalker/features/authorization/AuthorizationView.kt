1e30632887fb8da6a3ff7712ce061505b29ed5ee
DJ_Kovrik <djkovrik@gmail.com>
2017-09-25 20:27:06 +0300

    Define authorization view interface

---
+++ app/src/main/java/com/sedsoftware/yaptalker/features/authorization/AuthorizationView.kt
@@ -0,0 +1,19 @@
+package com.sedsoftware.yaptalker.features.authorization
+
+import com.arellomobile.mvp.MvpView
+import com.arellomobile.mvp.viewstate.strategy.OneExecutionStateStrategy
+import com.arellomobile.mvp.viewstate.strategy.StateStrategyType
+
+@StateStrategyType(OneExecutionStateStrategy::class)
+interface AuthorizationView : MvpView {
+
+  fun updateAppbarTitle()
+
+  fun loginSuccessMessage()
+
+  fun loginErrorMessage()
+
+  fun showErrorMessage(message: String)
+
+  fun signInButtonEnabled(enabled: Boolean)
+}
