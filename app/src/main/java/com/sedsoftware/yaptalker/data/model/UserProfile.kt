e73d5be0ee349337e5b0103e0a847d1d6089a906
DJ_Kovrik <djkovrik@gmail.com>
2017-09-30 13:51:32 +0300

    Parse user profile

---
+++ app/src/main/java/com/sedsoftware/yaptalker/data/model/UserProfile.kt
@@ -0,0 +1,42 @@
+package com.sedsoftware.yaptalker.data.model
+
+import pl.droidsonroids.jspoon.annotation.Selector
+
+class UserProfile {
+  @Selector("td:containsOwn(Группа) + td", defValue = "")
+  lateinit var group: String
+  @Selector("td:containsOwn(Статус) + td", defValue = "")
+  lateinit var status: String
+  @Selector(".uq > span")
+  lateinit var uq: String
+  @Selector("td:containsOwn(Подпись) + td", defValue = "", attr = "innerHtml")
+  lateinit var signature: String
+  @Selector("td:containsOwn(Награды) + td", defValue = "", attr = "innerHtml")
+  lateinit var rewards: String
+  @Selector("td:containsOwn(Регистрация) + td", defValue = "")
+  lateinit var registerDate: String
+  @Selector("td:containsOwn(Часовой пояс) + td", defValue = "")
+  lateinit var timeZone: String
+  @Selector("td:containsOwn(Вебсайт) + td", defValue = "")
+  lateinit var website: String
+  @Selector("td:containsOwn(Дата рождения) + td", defValue = "")
+  lateinit var birthDate: String
+  @Selector("td:containsOwn(Место жительства) + td", defValue = "")
+  lateinit var location: String
+  @Selector("td:containsOwn(Увлечения) + td", defValue = "", attr = "innerHtml")
+  lateinit var interests: String
+  @Selector("td:containsOwn(Половая принадлежность) + td", defValue = "")
+  lateinit var sex: String
+  @Selector("td:containsOwn(Всего полезных сообщений) + td", defValue = "0")
+  lateinit var messagesCount: String
+  @Selector("td:containsOwn(Сообщений в день) + td", defValue = "0")
+  lateinit var messsagesPerDay: String
+  @Selector("td:containsOwn(Всего баянов) + td", defValue = "0")
+  lateinit var bayans: String
+  @Selector("td:containsOwn(Создано тем сегодня) + td", defValue = "0")
+  lateinit var todayTopics: String
+  @Selector("td:containsOwn(E-mail + td", defValue = "-")
+  lateinit var email: String
+  @Selector("td:containsOwn(Номер ICQ) + td", defValue = "-")
+  lateinit var icq: String
+}
