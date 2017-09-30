d6135a6e952a4fd5e99e797b0128f39b0c799002
DJ_Kovrik <djkovrik@gmail.com>
2017-09-30 19:35:28 +0300

    Implement user profile controller

--- app/src/main/java/com/sedsoftware/yaptalker/features/userprofile/UserProfileController.kt
+++ app/src/main/java/com/sedsoftware/yaptalker/features/userprofile/UserProfileController.kt
@@ -55,13 +55,18 @@ class UserProfileController(val bundle: Bundle) : BaseController(bundle), UserPr
 
     view?.let {
       it.apply {
-        profile_group.text = String.format(Locale.getDefault(), context.stringRes(R.string.profile_group), profile.group)
-        profile_status.text = String.format(Locale.getDefault(), context.stringRes(R.string.profile_status), profile.status)
         profile_uq.ratingText = profile.uq
         profile_sign.textFromHtml(profile.signature)
         profile_rewards.textFromHtml(profile.rewards)
-        profile_registered.text = String.format(Locale.getDefault(), context.stringRes(R.string.profile_registered), profile.registerDate)
-        profile_time_zone.text = String.format(Locale.getDefault(), context.stringRes(R.string.profile_time_zone), profile.timeZone)
+
+        profile_group.text = String.format(Locale.getDefault(),
+            context.stringRes(R.string.profile_group), profile.group)
+        profile_status.text = String.format(Locale.getDefault(),
+            context.stringRes(R.string.profile_status), profile.status)
+        profile_registered.text = String.format(Locale.getDefault(),
+            context.stringRes(R.string.profile_registered), profile.registerDate)
+        profile_time_zone.text = String.format(Locale.getDefault(),
+            context.stringRes(R.string.profile_time_zone), profile.timeZone)
 
         if (profile.website.startsWith("http")) {
           val template = context.stringRes(R.string.profile_web_site)
@@ -69,16 +74,28 @@ class UserProfileController(val bundle: Bundle) : BaseController(bundle), UserPr
           profile_web_site.textFromHtml(html)
         }
 
-        profile_birth_date.text = String.format(Locale.getDefault(), context.stringRes(R.string.profile_birth_date), profile.birthDate)
-        profile_location.text = String.format(Locale.getDefault(), context.stringRes(R.string.profile_location), profile.location)
-        profile_interests.text = String.format(Locale.getDefault(), context.stringRes(R.string.profile_interests), profile.interests)
-        profile_sex.text = String.format(Locale.getDefault(), context.stringRes(R.string.profile_sex), profile.sex)
-        profile_messages.text = String.format(Locale.getDefault(), context.stringRes(R.string.profile_messages), profile.messagesCount)
-        profile_messages_day.text = String.format(Locale.getDefault(), context.stringRes(R.string.profile_messages_day), profile.messsagesPerDay)
-        profile_bayans.text = String.format(Locale.getDefault(), context.stringRes(R.string.profile_bayans), profile.bayans)
-        profile_topics_today.text = String.format(Locale.getDefault(), context.stringRes(R.string.profile_topics_today), profile.todayTopics)
-        profile_email.text = String.format(Locale.getDefault(), context.stringRes(R.string.profile_email), profile.email)
-        profile_icq.text = String.format(Locale.getDefault(), context.stringRes(R.string.profile_icq), profile.icq)
+        profile_birth_date.text = String.format(Locale.getDefault(),
+            context.stringRes(R.string.profile_birth_date), profile.birthDate)
+        profile_location.text = String.format(Locale.getDefault(),
+            context.stringRes(R.string.profile_location), profile.location)
+        profile_interests.text = String.format(Locale.getDefault(),
+            context.stringRes(R.string.profile_interests), profile.interests)
+        profile_sex.text = String.format(Locale.getDefault(),
+            context.stringRes(R.string.profile_sex), profile.sex)
+        profile_messages.text = String.format(Locale.getDefault(),
+            context.stringRes(R.string.profile_messages), profile.messagesCount)
+        profile_messages_day.text = String.format(Locale.getDefault(),
+            context.stringRes(R.string.profile_messages_day), profile.messsagesPerDay)
+        profile_bayans.text = String.format(Locale.getDefault(),
+            context.stringRes(R.string.profile_bayans), profile.bayans)
+        profile_topics_today.text = String.format(Locale.getDefault(),
+            context.stringRes(R.string.profile_topics_today), profile.todayTopics)
+        profile_email.text = String.format(Locale.getDefault(),
+            context.stringRes(R.string.profile_email), profile.email)
+        profile_icq.text = String.format(Locale.getDefault(),
+            context.stringRes(R.string.profile_icq), profile.icq)
+
+        //profile_avatar.loadFromUrl("http:${profile.avatar}")
 
         if (profile.photo.isNotEmpty()) {
           profile_photo.loadFromUrl(profile.photo)
