6a9949a3369b1f4a1edb7db163a81c5ddd399d0e
DJ_Kovrik <djkovrik@gmail.com>
2017-10-30 20:14:58 +0300

    Video activity tweaks

--- app/src/main/java/com/sedsoftware/yaptalker/features/videodisplay/VideoDisplayActivity.kt
+++ app/src/main/java/com/sedsoftware/yaptalker/features/videodisplay/VideoDisplayActivity.kt
@@ -1,12 +1,20 @@
 package com.sedsoftware.yaptalker.features.videodisplay
 
 import android.annotation.SuppressLint
+import android.content.res.Configuration
 import android.os.Bundle
+import android.view.View
+import android.webkit.WebChromeClient
+import android.webkit.WebView
 import com.arellomobile.mvp.presenter.InjectPresenter
 import com.sedsoftware.yaptalker.R
 import com.sedsoftware.yaptalker.base.BaseActivity
+import com.sedsoftware.yaptalker.commons.extensions.hideView
+import com.sedsoftware.yaptalker.commons.extensions.showView
+import com.sedsoftware.yaptalker.commons.extensions.toastError
 import kotlinx.android.synthetic.main.activity_video_display.*
 import kotlinx.android.synthetic.main.include_main_appbar.*
+
 
 class VideoDisplayActivity : BaseActivity(), VideoDisplayView {
 
@@ -17,15 +25,34 @@ class VideoDisplayActivity : BaseActivity(), VideoDisplayView {
     get() = R.layout.activity_video_display
 
   private val videoHtml: String by lazy {
-    intent.getStringExtra("video")
+    val iframe = intent
+        .getStringExtra("video")
+        .replace(Regex("width=\"\\d+\" height=\"\\d+\""), "")
+        .replace("src=\"//", "src=\"http://")
+
+    """
+    <html>
+    <head>
+      <link href="video.css" type="text/css" rel="stylesheet"/>
+    </head>
+      <body>
+        $iframe
+      </body>
+    </html>
+    """
   }
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
-    // TODO() Activity view requires styling
 
     setSupportActionBar(toolbar)
     supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+    if (resources.configuration.orientation == Configuration.ORIENTATION_LANDSCAPE) {
+      hideSystemUI()
+    } else {
+      showSystemUI()
+    }
   }
 
   override fun onResume() {
@@ -34,24 +61,50 @@ class VideoDisplayActivity : BaseActivity(), VideoDisplayView {
   }
 
   override fun showErrorMessage(message: String) {
-
+    toastError(message)
   }
 
   @SuppressLint("SetJavaScriptEnabled")
   override fun initWebView() {
     video_view?.settings?.javaScriptEnabled = true
     video_view?.settings?.setAppCacheEnabled(false)
+    video_view?.webChromeClient = WebChromeClient()
+    video_view?.settings?.loadWithOverviewMode = true
+    video_view?.settings?.useWideViewPort = true
+    video_view?.scrollBarStyle = WebView.SCROLLBARS_OUTSIDE_OVERLAY
+    video_view?.isScrollbarFadingEnabled = false
   }
 
   override fun displayWebViewContent() {
-    if (videoHtml.isNotEmpty()) {
-      video_view.loadDataWithBaseURL("http://www.yaplakal.com/", videoHtml,
-          "text/html; charset=UTF-8", null, null)
-    }
+    video_view.loadDataWithBaseURL("file:///android_asset/", videoHtml,
+        "text/html", "utf-8", null)
   }
 
   override fun clearWebView() {
     video_view?.clearHistory()
     video_view?.loadUrl("about:blank")
+  }
+
+  private fun hideSystemUI() {
+
+    appbar.hideView()
+
+    window.decorView.systemUiVisibility =
+        (View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+            or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+            or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+            or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+            or View.SYSTEM_UI_FLAG_FULLSCREEN
+            or View.SYSTEM_UI_FLAG_IMMERSIVE)
+  }
+
+  private fun showSystemUI() {
+
+    appbar.showView()
+
+    window.decorView.systemUiVisibility =
+        (View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+            or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+            or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN)
   }
 }
