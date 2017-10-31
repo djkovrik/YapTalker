a9378536856fc7d4f58e5d4c928be2ec9eb39f1b
DJ_Kovrik <djkovrik@gmail.com>
2017-10-31 20:18:24 +0300

    Implement adapters for active topics

---
+++ app/src/main/java/com/sedsoftware/yaptalker/features/activetopics/adapter/ActiveTopicsNavigationClickListener.kt
@@ -0,0 +1,14 @@
+package com.sedsoftware.yaptalker.features.activetopics.adapter
+
+interface ActiveTopicsNavigationClickListener {
+
+  fun onGoToFirstPageClick()
+
+  fun onGoToLastPageClick()
+
+  fun onGoToPreviousPageClick()
+
+  fun onGoToNextPageClick()
+
+  fun onGoToSelectedPageClick()
+}
