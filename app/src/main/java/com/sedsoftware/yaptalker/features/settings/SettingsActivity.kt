582fe7d6982579c8604a6a5847ad878125a4563a
DJ_Kovrik <djkovrik@gmail.com>
2017-11-19 19:05:20 +0300

    Tweak settings activity colors

--- app/src/main/java/com/sedsoftware/yaptalker/features/settings/SettingsActivity.kt
+++ app/src/main/java/com/sedsoftware/yaptalker/features/settings/SettingsActivity.kt
@@ -3,18 +3,27 @@ package com.sedsoftware.yaptalker.features.settings
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
-import com.arellomobile.mvp.MvpAppCompatActivity
+import com.sedsoftware.yaptalker.R
+import com.sedsoftware.yaptalker.base.BaseActivity
+import kotlinx.android.synthetic.main.include_main_appbar.*
 
-class SettingsActivity : MvpAppCompatActivity() {
+class SettingsActivity : BaseActivity() {
 
   companion object {
     fun getIntent(ctx: Context) = Intent(ctx, SettingsActivity::class.java)
   }
 
+  override val layoutId: Int
+    get() = R.layout.activity_settings
+
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
+
+    setSupportActionBar(toolbar)
+    supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
     fragmentManager.beginTransaction()
-        .replace(android.R.id.content, SettingsFragment())
+        .replace(R.id.settings_container, SettingsFragment())
         .commit()
   }
 }
