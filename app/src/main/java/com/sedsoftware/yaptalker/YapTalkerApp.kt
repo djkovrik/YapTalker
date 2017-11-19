737daecb9bca4b4cadae94289ba1e0c04c935c9a
DJ_Kovrik <djkovrik@gmail.com>
2017-11-19 14:44:44 +0300

    More color tweaks

--- app/src/main/java/com/sedsoftware/yaptalker/YapTalkerApp.kt
+++ app/src/main/java/com/sedsoftware/yaptalker/YapTalkerApp.kt
@@ -102,8 +102,8 @@ class YapTalkerApp : Application(), KodeinAware {
             DrawerImageLoader.Tags.PROFILE.name ->
               IconicsDrawable(ctx)
                   .icon(CommunityMaterial.Icon.cmd_account)
-                  .colorRes(R.color.colorPrimary)
-                  .backgroundColorRes(R.color.material_color_white_70_percent)
+                  .colorRes(R.color.colorGuestProfile)
+                  .backgroundColorRes(R.color.colorGuestProfileBackground)
                   .paddingDp(NAV_DRAWER_AVATAR_PADDING)
 
             else -> super.placeholder(ctx, tag)
