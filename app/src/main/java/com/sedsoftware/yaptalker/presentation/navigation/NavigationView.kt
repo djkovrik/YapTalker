3ef2465f91cf64310cf88038d296e1f0add88fe0
DJ_Kovrik <djkovrik@gmail.com>
2017-12-02 20:35:22 +0300

    Basic main screen implementation

---
+++ app/src/main/java/com/sedsoftware/yaptalker/presentation/navigation/NavigationView.kt
@@ -0,0 +1,8 @@
+package com.sedsoftware.yaptalker.presentation.navigation
+
+import com.arellomobile.mvp.viewstate.strategy.SkipStrategy
+import com.arellomobile.mvp.viewstate.strategy.StateStrategyType
+import com.sedsoftware.yaptalker.base.BaseView
+
+@StateStrategyType(SkipStrategy::class)
+interface NavigationView : BaseView
