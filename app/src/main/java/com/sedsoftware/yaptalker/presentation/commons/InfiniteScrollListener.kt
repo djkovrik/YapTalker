25d3ca0a28934f0c3b1c1381bf2ca61d0b55f8f0
DJ_Kovrik <djkovrik@gmail.com>
2017-12-12 22:22:02 +0300

    Restore commons

---
+++ app/src/main/java/com/sedsoftware/yaptalker/presentation/commons/InfiniteScrollListener.kt
@@ -0,0 +1,42 @@
+package com.sedsoftware.yaptalker.commons
+
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
+
+class InfiniteScrollListener(
+    private val func: () -> Unit,
+    val layoutManager: LinearLayoutManager) : RecyclerView.OnScrollListener() {
+
+  companion object {
+    private const val VISIBLE_THRESHOLD = 4
+  }
+
+  private var previousTotal = 0
+  private var loading = true
+  private var visibleThreshold = VISIBLE_THRESHOLD
+  private var firstVisibleItem = 0
+  private var visibleItemCount = 0
+  private var totalItemCount = 0
+
+  override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
+    super.onScrolled(recyclerView, dx, dy)
+
+    if (dy > 0) {
+      visibleItemCount = recyclerView.childCount
+      totalItemCount = layoutManager.itemCount
+      firstVisibleItem = layoutManager.findFirstVisibleItemPosition()
+
+      if (loading) {
+        if (totalItemCount > previousTotal) {
+          loading = false
+          previousTotal = totalItemCount
+        }
+      }
+      if (!loading && (totalItemCount - visibleItemCount)
+          <= (firstVisibleItem + visibleThreshold)) {
+        func()
+        loading = true
+      }
+    }
+  }
+}
---
+++ app/src/main/java/com/sedsoftware/yaptalker/presentation/commons/PicassoImageGetter.kt
@@ -0,0 +1,49 @@
+package com.sedsoftware.yaptalker.commons
+
+import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.Canvas
+import android.graphics.drawable.BitmapDrawable
+import android.graphics.drawable.Drawable
+import android.text.Html
+import android.widget.TextView
+import com.sedsoftware.yaptalker.presentation.commons.extensions.currentDensity
+import com.squareup.picasso.Picasso
+import com.squareup.picasso.Target as DrawableTarget
+
+class PicassoImageGetter(val context: Context, val textView: TextView) : Html.ImageGetter {
+
+  override fun getDrawable(src: String?): Drawable {
+    val drawable = EmojiDrawable()
+    Picasso.with(context).load("http:$src").into(drawable)
+    return drawable
+  }
+
+  inner class EmojiDrawable : DrawableTarget, BitmapDrawable(context.resources, "") {
+
+    var drawable: Drawable? = null
+
+    override fun draw(canvas: Canvas) {
+      drawable?.draw(canvas)
+    }
+
+    override fun onPrepareLoad(placeHolderDrawable: Drawable?) {
+    }
+
+    override fun onBitmapFailed(errorDrawable: Drawable?) {
+    }
+
+    override fun onBitmapLoaded(bitmap: Bitmap?, from: Picasso.LoadedFrom?) {
+      setTargetDrawable(BitmapDrawable(context.resources, bitmap))
+    }
+
+    private fun setTargetDrawable(drawable: Drawable) {
+      this.drawable = drawable
+      val width = drawable.intrinsicWidth * context.currentDensity
+      val height = drawable.intrinsicHeight * context.currentDensity
+      drawable.setBounds(0, 0, width, height)
+      setBounds(0, 0, width, height)
+      textView.text = textView.text
+    }
+  }
+}
