3bc876f63049978ab2ebe924f03bb0d5d36505ba
DJ_Kovrik <djkovrik@gmail.com>
2018-02-17 20:56:29 +0300

    Create custom file path resolver

---
+++ device/src/main/java/com/sedsoftware/yaptalker/device/fileresolver/FilePathResolver.kt
@@ -0,0 +1,89 @@
+package com.sedsoftware.yaptalker.device.fileresolver
+
+import android.content.ContentUris
+import android.content.Context
+import android.database.Cursor
+import android.net.Uri
+import android.os.Environment
+import android.provider.DocumentsContract
+import android.provider.MediaStore
+import javax.inject.Inject
+
+class FilePathResolver @Inject constructor(private val context: Context) {
+
+  fun getFilePathFromUri(uri: Uri): String? {
+
+    if (DocumentsContract.isDocumentUri(context, uri)) {
+      if (isExternalStorageDocument(uri)) {
+        val docId = DocumentsContract.getDocumentId(uri)
+        val split = docId.split(":".toRegex()).dropLastWhile { it.isEmpty() }.toTypedArray()
+        val type = split[0]
+
+        if ("primary".equals(type, ignoreCase = true)) {
+          return "${Environment.getExternalStorageDirectory()}/${split[1]}"
+        }
+
+      } else if (isDownloadsDocument(uri)) {
+
+        val id = DocumentsContract.getDocumentId(uri)
+        val contentUri = ContentUris.withAppendedId(
+          Uri.parse("content://downloads/public_downloads"), java.lang.Long.valueOf(id)!!
+        )
+        return getDataColumn(contentUri, null, null)
+      } else if (isMediaDocument(uri)) {
+        val docId = DocumentsContract.getDocumentId(uri)
+        val split = docId.split(":".toRegex()).dropLastWhile { it.isEmpty() }.toTypedArray()
+        val type = split[0]
+
+        var contentUri: Uri? = null
+        when (type) {
+          "image" -> contentUri = MediaStore.Images.Media.EXTERNAL_CONTENT_URI
+          "video" -> contentUri = MediaStore.Video.Media.EXTERNAL_CONTENT_URI
+          "audio" -> contentUri = MediaStore.Audio.Media.EXTERNAL_CONTENT_URI
+        }
+
+        val selection = "_id=?"
+        val selectionArgs = arrayOf(split[1])
+
+        return getDataColumn(contentUri, selection, selectionArgs)
+      }
+    } else if ("content".equals(uri.scheme, ignoreCase = true)) {
+      return getDataColumn(uri, null, null)
+    } else if ("filePath".equals(uri.scheme, ignoreCase = true)) {
+      return uri.path
+    }
+
+    return null
+  }
+
+  private fun getDataColumn(uri: Uri?, selection: String?, selectionArgs: Array<String>?): String? {
+
+    var cursor: Cursor? = null
+    val column = "_data"
+    val projection = arrayOf(column)
+
+    try {
+      cursor = context.contentResolver.query(uri, projection, selection, selectionArgs, null)
+      if (cursor != null && cursor.moveToFirst()) {
+        val columnIndex = cursor.getColumnIndexOrThrow(column)
+        return cursor.getString(columnIndex)
+      }
+    } finally {
+      if (cursor != null)
+        cursor.close()
+    }
+    return null
+  }
+
+  private fun isExternalStorageDocument(uri: Uri): Boolean {
+    return "com.android.externalstorage.documents" == uri.authority
+  }
+
+  private fun isDownloadsDocument(uri: Uri): Boolean {
+    return "com.android.providers.downloads.documents" == uri.authority
+  }
+
+  private fun isMediaDocument(uri: Uri): Boolean {
+    return "com.android.providers.media.documents" == uri.authority
+  }
+}
