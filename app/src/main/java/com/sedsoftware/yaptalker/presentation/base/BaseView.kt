e706d5ffa5c338edc95a93f3039e8fe239a82e51
DJ_Kovrik <djkovrik@gmail.com>
2018-03-04 19:38:43 +0300

    Test loading indicator strategy tweak

--- app/src/main/java/com/sedsoftware/yaptalker/presentation/base/BaseView.kt
+++ app/src/main/java/com/sedsoftware/yaptalker/presentation/base/BaseView.kt
@@ -5,16 +5,17 @@ import com.arellomobile.mvp.viewstate.strategy.AddToEndSingleStrategy
 import com.arellomobile.mvp.viewstate.strategy.SkipStrategy
 import com.arellomobile.mvp.viewstate.strategy.StateStrategyType
 
-@StateStrategyType(AddToEndSingleStrategy::class)
+@StateStrategyType(SkipStrategy::class)
 interface BaseView : MvpView {
 
-  @StateStrategyType(SkipStrategy::class)
   fun showErrorMessage(message: String)
 
+  @StateStrategyType(value = AddToEndSingleStrategy::class, tag = "LoadingIndicator")
   fun showLoadingIndicator() {
     // Default empty implementation
   }
 
+  @StateStrategyType(value = AddToEndSingleStrategy::class, tag = "LoadingIndicator")
   fun hideLoadingIndicator() {
     // Default empty implementation
   }
