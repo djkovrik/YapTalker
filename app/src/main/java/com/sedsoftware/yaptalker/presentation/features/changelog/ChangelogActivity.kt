62f95dbddc2ca9233df2d9253fd5e556f105ac7a
DJ_Kovrik <djkovrik@gmail.com>
2018-03-31 21:45:05 +0300

    Fix changelog activity back button

--- app/src/main/java/com/sedsoftware/yaptalker/presentation/features/changelog/ChangelogActivity.kt
+++ app/src/main/java/com/sedsoftware/yaptalker/presentation/features/changelog/ChangelogActivity.kt
@@ -3,9 +3,6 @@ package com.sedsoftware.yaptalker.presentation.features.changelog
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
-import android.os.PersistableBundle
-import android.support.v4.app.NavUtils
-import android.view.MenuItem
 import com.arellomobile.mvp.presenter.InjectPresenter
 import com.arellomobile.mvp.presenter.ProvidePresenter
 import com.sedsoftware.yaptalker.R
@@ -34,22 +31,11 @@ class ChangelogActivity : BaseActivity(), ChangelogView {
   @ProvidePresenter
   fun providePresenter() = presenter
 
-  override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
-    super.onCreate(savedInstanceState, persistentState)
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
 
     setSupportActionBar(toolbar)
-    supportActionBar?.setDisplayShowHomeEnabled(true)
     supportActionBar?.setDisplayHomeAsUpEnabled(true)
-  }
-
-  override fun onOptionsItemSelected(item: MenuItem): Boolean {
-    when (item.itemId) {
-      android.R.id.home -> {
-        NavUtils.navigateUpFromSameTask(this)
-        return true
-      }
-    }
-    return super.onOptionsItemSelected(item)
   }
 
   override fun displayChangelog(markdown: String) {
