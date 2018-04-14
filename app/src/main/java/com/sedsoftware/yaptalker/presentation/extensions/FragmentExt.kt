974c76d0ba8a47a04f4394573f331fbad8386b38
DJ_Kovrik <djkovrik@gmail.com>
2018-04-14 12:30:45 +0300

    Replace common colors with theme specific attrs

--- app/src/main/java/com/sedsoftware/yaptalker/presentation/extensions/FragmentExt.kt
+++ app/src/main/java/com/sedsoftware/yaptalker/presentation/extensions/FragmentExt.kt
@@ -1,35 +1,35 @@
 package com.sedsoftware.yaptalker.presentation.extensions
 
-import android.support.annotation.ColorRes
+import android.support.annotation.AttrRes
 import android.support.v4.app.Fragment
 import android.widget.TextView
 import com.androidadvance.topsnackbar.TSnackbar
 import com.sedsoftware.yaptalker.R
 
 fun Fragment.snackError(message: String) {
-  showTopSnackbar(message, R.color.snackColorError, R.color.snackColorText)
+  showTopSnackbar(message, R.attr.snackColorError, R.attr.snackColorText)
 }
 
 fun Fragment.snackSuccess(message: String) {
-  showTopSnackbar(message, R.color.snackColorSuccess, R.color.snackColorText)
+  showTopSnackbar(message, R.attr.snackColorSuccess, R.attr.snackColorText)
 }
 
 fun Fragment.snackInfo(message: String) {
-  showTopSnackbar(message, R.color.snackColorInfo, R.color.snackColorText)
+  showTopSnackbar(message, R.attr.snackColorInfo, R.attr.snackColorText)
 }
 
 fun Fragment.snackWarning(message: String) {
-  showTopSnackbar(message, R.color.snackColorWarning, R.color.snackColorText)
+  showTopSnackbar(message, R.attr.snackColorWarning, R.attr.snackColorText)
 }
 
-fun Fragment.showTopSnackbar(message: String, @ColorRes bgColor: Int, @ColorRes textColor: Int) {
+fun Fragment.showTopSnackbar(message: String, @AttrRes bgColor: Int, @AttrRes textColor: Int) {
   activity?.let { activity ->
     TSnackbar
       .make(activity.findViewById(R.id.content_container), message, TSnackbar.LENGTH_SHORT)
-      .also { snackbar -> snackbar.view.setBackgroundColor(activity.color(bgColor)) }
+      .also { snackbar -> snackbar.view.setBackgroundColor(activity.getColorFromAttr(bgColor)) }
       .also { snackbar ->
         (snackbar.view.findViewById(com.androidadvance.topsnackbar.R.id.snackbar_text) as? TextView)
-          ?.setTextColor(activity.color(textColor))
+          ?.setTextColor(activity.getColorFromAttr(textColor))
       }
       .show()
   }
--- app/src/main/java/com/sedsoftware/yaptalker/presentation/extensions/TextViewExt.kt
+++ app/src/main/java/com/sedsoftware/yaptalker/presentation/extensions/TextViewExt.kt
@@ -7,7 +7,7 @@ import com.sedsoftware.yaptalker.presentation.custom.PicassoImageGetter
 
 var TextView.textColor: Int
   get() = currentTextColor
-  set(v) = setTextColor(context.color(v))
+  set(v) = setTextColor(context.getColorFromAttr(v))
 
 @Suppress("MagicNumber")
 fun TextView.loadRatingBackground(rating: Int) {
@@ -15,32 +15,32 @@ fun TextView.loadRatingBackground(rating: Int) {
   when (rating) {
   // Platinum
     in 1000..50000 -> {
-      textColor = R.color.colorRatingPlatinumText
+      textColor = R.attr.colorRatingPlatinumText
       setBackgroundResource(R.drawable.topic_rating_platinum)
     }
   // Gold
     in 500..999 -> {
-      textColor = R.color.colorRatingGoldText
+      textColor = R.attr.colorRatingGoldText
       setBackgroundResource(R.drawable.topic_rating_gold)
     }
   // Green
     in 50..499 -> {
-      textColor = R.color.colorRatingGreenText
+      textColor = R.attr.colorRatingGreenText
       setBackgroundResource(R.drawable.topic_rating_green)
     }
   // Gray
     in -9..49 -> {
-      textColor = R.color.colorRatingGreyText
+      textColor = R.attr.colorRatingGreyText
       setBackgroundResource(R.drawable.topic_rating_grey)
     }
   // Red
     in -99..-10 -> {
-      textColor = R.color.colorRatingRedText
+      textColor = R.attr.colorRatingRedText
       setBackgroundResource(R.drawable.topic_rating_red)
     }
   // Dark Red
     else -> {
-      textColor = R.color.colorRatingDarkRedText
+      textColor = R.attr.colorRatingDarkRedText
       setBackgroundResource(R.drawable.topic_rating_dark_red)
     }
   }
