06dadd89b3234dfb7b390176158f04b7e2800bed
DJ_Kovrik <djkovrik@gmail.com>
2018-04-21 20:40:25 +0300

    Add fragment extensions

---
+++ app/src/main/java/com/sedsoftware/yaptalker/presentation/extensions/FragmentExt.kt
@@ -0,0 +1,7 @@
+package com.sedsoftware.yaptalker.presentation.extensions
+
+import android.support.annotation.StringRes
+import android.support.v4.app.Fragment
+
+fun Fragment.string(@StringRes resId: Int): String =
+  context?.string(resId) ?: ""
