aaac6b5f8d90b7ee9d01f3967aa4f9d7c8a1bb7e
DJ_Kovrik <djkovrik@gmail.com>
2018-04-23 17:09:58 +0300

    Create message sending interactor

---
+++ domain/src/main/java/com/sedsoftware/yaptalker/domain/interactor/MessagePostingInteractor.kt
@@ -0,0 +1,31 @@
+package com.sedsoftware.yaptalker.domain.interactor
+
+import com.sedsoftware.yaptalker.domain.repository.ChosenTopicRepository
+import io.reactivex.Completable
+import javax.inject.Inject
+
+class MessagePostingInteractor @Inject constructor(
+  private val chosenTopicRepository: ChosenTopicRepository
+) {
+
+  fun sendMessageRequest(forumId: Int,
+                         topicId: Int,
+                         page: Int,
+                         authKey: String,
+                         message: String,
+                         filePath: String = ""): Completable =
+
+    chosenTopicRepository
+      .requestMessageSending(forumId, topicId, page, authKey, message, filePath)
+
+  fun sendEditedMessageRequest(forumId: Int,
+                               topicId: Int,
+                               postId: Int,
+                               page: Int,
+                               authKey: String,
+                               message: String,
+                               file: String = ""): Completable =
+
+    chosenTopicRepository
+      .requestEditedMessageSending(forumId, topicId, postId, page, authKey, message, file)
+}
