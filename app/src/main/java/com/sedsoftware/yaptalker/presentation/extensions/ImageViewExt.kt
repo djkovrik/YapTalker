4f78244a94efafd9d016dc3aa9e03dff46a7d9f5
DJ_Kovrik <djkovrik@gmail.com>
2018-06-05 20:55:18 +0300

    Gif displaying tweak

--- app/src/main/java/com/sedsoftware/yaptalker/presentation/extensions/ImageViewExt.kt
+++ app/src/main/java/com/sedsoftware/yaptalker/presentation/extensions/ImageViewExt.kt
@@ -2,6 +2,7 @@ package com.sedsoftware.yaptalker.presentation.extensions
 
 import android.widget.ImageView
 import com.bumptech.glide.Glide
+import com.bumptech.glide.load.engine.DiskCacheStrategy.SOURCE
 import com.sedsoftware.yaptalker.presentation.custom.CircleImageTransformation
 import com.sedsoftware.yaptalker.presentation.custom.RoundedTransformation
 import com.squareup.picasso.Picasso
@@ -17,6 +18,8 @@ fun ImageView.loadFromUrlWithGifSupport(url: String) {
   Glide
     .with(context)
     .load(url.validateUrl())
+    .diskCacheStrategy(SOURCE)
+    .crossFade()
     .into(this)
 }
 
