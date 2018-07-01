4ef43679e322897351e2eb1cbc3370d1947b4ccb
DJ_Kovrik <djkovrik@gmail.com>
2018-07-01 09:33:33 +0300

    News and incubator parsing fixes

--- data/src/main/java/com/sedsoftware/yaptalker/data/parsed/IncubatorPageParsed.kt
+++ data/src/main/java/com/sedsoftware/yaptalker/data/parsed/IncubatorPageParsed.kt
@@ -6,10 +6,10 @@ import pl.droidsonroids.jspoon.annotation.Selector
  * Class which represents parsed incubator page in data layer.
  */
 class IncubatorPageParsed {
-  @Selector(value = "td.newshead")
-  lateinit var headers: List<IncubatorHead>
-  @Selector(value = "td.postcolor")
-  lateinit var contents: List<IncubatorContent>
-  @Selector(value = "td.holder")
-  lateinit var bottoms: List<IncubatorBottom>
+  @Selector(value = "td[class=newshead][id^=topic_]")
+  var headers: List<IncubatorHead> = emptyList()
+  @Selector(value = "td[class=postcolor news-content][id^=news_]")
+  var contents: List<IncubatorContent> = emptyList()
+  @Selector(value = "td[class=holder newsbottom]")
+  var bottoms: List<IncubatorBottom> = emptyList()
 }
