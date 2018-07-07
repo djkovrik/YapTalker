754b57f072fcd3376ba2d4cb61bcd54e09dab262
DJ_Kovrik <djkovrik@gmail.com>
2018-07-07 13:20:29 +0300

    Revert db change

--- data/src/main/java/com/sedsoftware/yaptalker/data/database/YapTalkerDatabase.kt
+++ data/src/main/java/com/sedsoftware/yaptalker/data/database/YapTalkerDatabase.kt
@@ -4,17 +4,21 @@ import android.arch.persistence.room.Database
 import android.arch.persistence.room.RoomDatabase
 import android.arch.persistence.room.TypeConverters
 import com.sedsoftware.yaptalker.data.database.converter.DateConverter
+import com.sedsoftware.yaptalker.data.database.dao.BlacklistedTagDao
 import com.sedsoftware.yaptalker.data.database.dao.BlacklistedTopicDao
+import com.sedsoftware.yaptalker.data.database.model.BlacklistedTagDbModel
 import com.sedsoftware.yaptalker.data.database.model.BlacklistedTopicDbModel
 
-@Database(entities = [(BlacklistedTopicDbModel::class)], version = 2)
+@Database(entities = [(BlacklistedTagDbModel::class), (BlacklistedTopicDbModel::class)], version = 1)
 @TypeConverters(DateConverter::class)
 abstract class YapTalkerDatabase : RoomDatabase() {
 
-    companion object {
-        const val DATABASE_NAME = "yaptalker_db"
-        const val TOPICS_BLACKLIST_TABLE = "blacklist_topics"
-    }
+  companion object {
+    const val DATABASE_NAME = "yaptalker_db"
+    const val TAGS_BLACKLIST_TABLE = "blacklist_tags"
+    const val TOPICS_BLACKLIST_TABLE = "blacklist_topics"
+  }
 
-    abstract fun getTopicsDao(): BlacklistedTopicDao
+  abstract fun getTagsDao(): BlacklistedTagDao
+  abstract fun getTopicsDao(): BlacklistedTopicDao
 }
---
+++ data/src/main/java/com/sedsoftware/yaptalker/data/database/model/BlacklistedTagDbModel.kt
@@ -0,0 +1,16 @@
+package com.sedsoftware.yaptalker.data.database.model
+
+import android.arch.persistence.room.ColumnInfo
+import android.arch.persistence.room.Entity
+import android.arch.persistence.room.PrimaryKey
+import com.sedsoftware.yaptalker.data.database.YapTalkerDatabase
+
+@Entity(tableName = YapTalkerDatabase.TAGS_BLACKLIST_TABLE)
+data class BlacklistedTagDbModel(
+    @PrimaryKey(autoGenerate = true)
+    var id: Long = 0,
+    @ColumnInfo(name = "tag_name")
+    var tagName: String = "",
+    @ColumnInfo(name = "tag_link")
+    var tagLink: String = ""
+)
