4aab6b4e094d233360d414fca53d71c0197e514c
DJ_Kovrik <djkovrik@gmail.com>
2018-07-19 20:29:55 +0300

    Tweak saved topic state values

--- device/src/main/java/com/sedsoftware/yaptalker/device/storage/state/TopicState.kt
+++ device/src/main/java/com/sedsoftware/yaptalker/device/storage/state/TopicState.kt
@@ -2,4 +2,8 @@ package com.sedsoftware.yaptalker.device.storage.state
 
 import android.os.Parcelable
 
-class TopicState(val scrollState: Parcelable, val currentPage: Int, val totalPages: Int, val label: String)
+class TopicState(
+    val forumId: Int,
+    val topicId: Int,
+    val currentPage: Int,
+    val scrollState: Parcelable)
